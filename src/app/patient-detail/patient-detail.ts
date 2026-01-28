dacc471f5194ac810e9eb5c357672565ca99fd1d
arshiamunir291 <arshiamunir786@gmail.com>
2026-01-28 03:59:03 -0500

    Create project,its components and service

---
+++ src/app/patient-detail/patient-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-detail',
+  imports: [],
+  templateUrl: './patient-detail.html',
+  styleUrl: './patient-detail.css',
+})
+export class PatientDetail {
+
+}
